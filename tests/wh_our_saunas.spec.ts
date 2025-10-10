3931e253a1b290b8a2abd8c25a6a07ed9e89777b
Rob Bennett <rob.contract@gmail.com>
2025-10-10 17:24:18 +0100

    Trying to fix cookie issue

--- tests/wh_our_saunas.spec.ts
+++ tests/wh_our_saunas.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe("Navigation from 'Our Saunas'", () => {
-test('To OHK', async ({ page }) => {
+test('To the  saunas', async ({ page }) => {
   await page.goto('https://www.wildhut.com/');
 
   //accept cookies
